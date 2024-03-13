f7d222c056d7814896f1bda63298258b1a37abeb
MQasim34 <mq30994@gmail.com>
2024-03-13 10:23:55 +0500

    added: distt folder

--- src/utils/getBlogcontent.js
+++ src/utils/getBlogcontent.js
@@ -2,7 +2,7 @@
 
 export const getBlogcontent = async() => {
 
-    const response  = await fetch('./src/constant/bolgContent/data.json');
+    const response  = await fetch('/BuzzStrivePro/src/constant/bolgContent/data.json');
     const data = await response.json();
   return data; 
 }
