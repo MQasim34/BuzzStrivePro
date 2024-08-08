c9644ad5f8619918433a2af10ae3eb6d3577fb84
M.Qasim <116056476+MQasim34@users.noreply.github.com>
2024-08-08 10:37:16 -0700

    Update works.js

--- src/constant/works/works.js
+++ src/constant/works/works.js
@@ -4,7 +4,7 @@ export const workDetail = [
     {
          id: 11,
         title: "Taxi theory test",
-        details: "I developed a comprehensive "Taxi Theory Test" website in WordPress, featuring multi-language support. The platform includes custom quiz functionality, time-bound assessments, and interactive feedback, providing an efficient learning environment for aspiring taxi drivers.",
+        details: "I developed a comprehensive 'Taxi Theory Test' website in WordPress, featuring multi-language support. The platform includes custom quiz functionality, time-bound assessments, and interactive feedback, providing an efficient learning environment for aspiring taxi drivers.",
         imgAlt: "taxi theory test img",
         imgUrl: learnworlds,
         categories: "Wordpress",
